72695c31bfcf66e08bc64b2000c3a89071e4d5c1
sharlq <shehabakhader@gmail.com>
2021-04-28 12:19:50 +0300

    started the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react'
+import {BrowserRouter as Router , Route} from "react-router-dom"
+import Chat from "./components/Chat"
+import Join from "./components/Join"
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router className="App">
+     <Route path="/" exact component={Join} />
+     <Route path="/chat"  component={Chat} />
+    </Router>
   );
 }
 
